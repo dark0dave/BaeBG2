5fedab8710cc6977baf193025b4aa2610a9aa8e0
dark0dave <dark0dave@mykolab.com>
2024-12-16 00:12:06 +0000

    fix(vars): Dialogue still not triggering
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- 0dbae/dialog/0dbae25b.d
+++ 0dbae/dialog/0dbae25b.d
@@ -4,13 +4,14 @@ BEGIN 0DBAE25B
 CHAIN
 IF ~InParty("0DBAE25")
     See("0DBAE25")
-    See("Sarevok")
+    CombatCounter(0)
+    !See([ENEMY])
     !StateCheck("Sarevok", CD_STATE_NOTVALID)
     !StateCheck("0DBAE25", CD_STATE_NOTVALID)
-    Global("0DBAE25BSarevok1","GLOBAL",0)~
-THEN BSAREV25 0DBAE25BSarevok1
+    Global("0DBAE25BSARV1","GLOBAL",0)~
+THEN BSAREV25 0DBAE25BSARV1
 @6000 /* Drow, the more I know you the more I wish I had found you sooner. */
-DO ~SetGlobal("0DBAE25BSarevok1","GLOBAL",1)~
+DO ~SetGlobal("0DBAE25BSARV1","GLOBAL",1)~
 == 0DBAE25B @6001 /* Ooh! What wonderful words! */
 == BSAREV25 @6002 /* I sought conquer and death on a massive scale to bring about the return of my true father - Bhaal, Lord of Murder. You sought to conquer nations to exploit their slaves for entertainment. */
 == 0DBAE25B @6003 /* -Nations?- Nah. I was jolly enough with just surly slaves and gladiators glad to get another round to fight. */
