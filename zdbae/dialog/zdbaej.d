8bf9d6a221f78674baf20237e5fb3656a797db61
bucketfulofsunshine <135817175+bucketfulofsunshine@users.noreply.github.com>
2025-01-04 14:03:55 +0200

    feat(boots): Add boot lines
    - Added a dialogue line, and script, for when Baeloth has the Luxurious Lizardskin Leapers.
    - No TP2 changes to compile either file yet.

--- zdbae/dialog/zdbaej.d
+++ zdbae/dialog/zdbaej.d
@@ -1,1 +1,8 @@
 BEGIN ZDBAEJ
+
+/* luxurious lizardskin leapers are equipped */
+IF ~Global("zdbaeleapers","GLOBAL",1)~
+THEN zdbaeleapers
+  SAY @7000 /* Ah, the Luxurious Lizardskin Leapers! A splendid symphony of supple skin and sublime style. With these I shall sashay forward and even the stars shall seem subdued in comparison! */
+  IF ~~ THEN DO ~SetGlobal("zdbaeleapers","GLOBAL",2)~ EXIT
+END
