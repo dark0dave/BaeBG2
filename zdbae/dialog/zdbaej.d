fcf36f79f7761edd2004c78180825afe88e8b7e9
dark0dave <dark0dave@mykolab.com>
2025-01-22 20:18:00 +0000

    feat(gaal): Interjections for gaal temple district
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- zdbae/dialog/zdbaej.d
+++ zdbae/dialog/zdbaej.d
@@ -27,3 +27,20 @@ THEN zdbaefurries
   SAY @7003 /* Splendidly styled but too subtle for my taste. I dazzle, I delight. I refuse to be swallowed by the shadows like some sullen serf. */
   IF ~~ THEN DO ~SetGlobal("zdbaefurries","GLOBAL",2)~ EXIT
 END
+
+// Temple District (AR0900)
+// Response: Listen to me, my brothers and sisters... heed my words. We have been chosen as the recipient of a most holy miracle, one that should neither be dismissed nor ignored!
+INTERJECT CSGAAL 0 ZDBAEGAAL0
+  == ZDBAEJ IF ~InParty("ZDBAE") !StateCheck("ZDBAE", CD_STATE_NOTVALID)~ THEN
+  @4050 /* Ohhh oh! This should be good! */
+END CSGAAL 1
+// Response: I say that the churches lie to you! They claim their gods are present when they do nothing other than require your coin to fill their pockets!
+INTERJECT_COPY_TRANS CSGAAL 3 ZDBAEGAAL1
+  == ZDBAEJ IF ~InParty("ZDBAE") !StateCheck("ZDBAE", CD_STATE_NOTVALID)~ THEN
+  @4051 /* And I, I say listen to me Baeloth the Bright! Baeloth the Bedazzling!  Baeloth the ... Well in any event, don't listen to this blind bat, however brilliant his bids at bewilderment are */
+END
+// Response: Come, then! Those of you who wish to become the truly faithful, to witness the magnificence of the Unseeing Eye and the truth he brings... come with me!
+INTERJECT_COPY_TRANS CSGAAL 10 ZDBAEGAAL10
+  == ZDBAEJ IF ~InParty("ZDBAE") !StateCheck("ZDBAE", CD_STATE_NOTVALID)~ THEN
+  @4052 /* Follow him to mediocrity or follow me to magnificence. Your choice, of course. */
+END
