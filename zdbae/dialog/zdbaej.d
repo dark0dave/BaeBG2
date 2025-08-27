206f50ec8442354b1109132b24572c5e6ab26099
bucketfulofsunshine <AEShadowHunter@gmail.com>
2025-08-27 19:12:34 +0200

    MOAR!
    - Fixed BAM description sizes.
    - Fixed wrong banter popping up for DA BOOTS.
    - Edited out the banter that doesn't have corresponding boots.
    - Added a quick interjection during Aerie's intro (still a WIP).
    - Added a quick dialogue upon entering Waukeen's Promenade (starting Real Housewives).
    - Added journal entry for said quest.

--- zdbae/dialog/zdbaej.d
+++ zdbae/dialog/zdbaej.d
@@ -1,5 +1,18 @@
 BEGIN ZDBAEJ
 
+/* real housewives of athkatla intro */
+IF ~Global("ZDBaeBoots","GLOBAL",1)~ THEN ZDBAETOSSACOINTOYOURBAELOTH
+SAY ~The bustle of the bazaar! Trinkets, treasures, and tantalizing wares aplenty. My boots, alas, are worn thin from all our wandering. Perhaps you should spend a coin or two and preserve your most precious pal?~
+++ ~All of that postering just to tell me your boots are falling apart!~ + ZDBAEBOOTS1 
+++ ~Then I suppose we could buy you a new pair.~ + ZDBAEBOOTS1
+++ ~You're here to sling spells, Baeloth, not bore me with your endless chatter.~ DO ~SetGlobal("ZDBAEBoots","GLOBAL",2)~ EXIT
+END
+
+IF ~~ ZDBAEBOOTS1
+SAY ~Precisely! What greater tragedy is there than Baeloth Barrityl being undone by shabby soles?~
+IF ~~ DO ~AddJournalEntry(@1000001, QUEST) SetGlobal("ZDBAEBoots","GLOBAL",2)~ EXIT
+END
+
 /* item-based talks */
 /* luxurious lizardskin leapers are equipped */
 IF ~Global("zdbaeleapers","GLOBAL",1)~
@@ -16,11 +29,12 @@ THEN zdbaesturdy
 END
 
 /* simple boots are equipped - how DARE you! */
-IF ~Global("zdbaesimple","GLOBAL",1)~
-THEN zdbaesimple
-  SAY @7002 /* Blast these brutish boots! They are bereft of beauty and bland in design. They barely beguile the eye! Why am I, the brilliant Baeloth, forced to travel with such foul footwear? */
-  IF ~~ THEN DO ~SetGlobal("zdbaesimple","GLOBAL",2)~ EXIT
-END
+// commented out because this item doesn't exist
+//  IF ~Global("zdbaesimple","GLOBAL",1)~
+// THEN zdbaesimple
+//  SAY @7002 /* Blast these brutish boots! They are bereft of beauty and bland in design. They barely beguile the eye! Why am I, the brilliant Baeloth, forced to travel with such foul footwear? */
+//  IF ~~ THEN DO ~SetGlobal("zdbaesimple","GLOBAL",2)~ EXIT
+// END
 
 /* boots with da fur are equipped */
 IF ~Global("zdbaefurries","GLOBAL",1)~
@@ -69,6 +83,13 @@ INTERJECT_COPY_TRANS CSGAAL 10 ZDBAEGAAL10
   @4052 /* Follow him to mediocrity or follow me to magnificence. Your choice, of course. */
 END
 
+// Market District
+// Circus, Aerie introduction
+// Thanks for being vague when telling us you sense something is off, Baeloth
+I_C_T AERIE 30 ZDBAECIRC1
+== ZDBAEJ IF ~InParty("ZDBAE") !StateCheck("ZDBAE",CD_STATE_NOTVALID) InMyArea("ZDBAE")~ THEN ~The Weave twists and twirls around her. Such chaotic patterns require a cunning conductor. But, surely, that is why you keep *me* close.~ 
+END
+
 // Docks District
 // Buy Jaheria a gift (dshop02.d)
 // ~<CHARNAME>! You do not need to...
@@ -83,32 +104,32 @@ END
 // Spellhold
 // Slayer Change
 I_C_T PLAYER1 3 ZDBAESLAY1
-== ZDBAEJ IF ~InParty("ZDBAE") !StateCheck("ZDBAE",CD_STATE_NOTVALID) InMyArea("ZDBAE")~ THEN @10145 /* Bravo! Such a savage spectacle! I call for an encore! */
+== ZDBAEJ IF ~InParty("ZDBAE") !StateCheck("ZDBAE",CD_STATE_NOTVALID) InMyArea("ZDBAE")~ THEN ~Bravo! Such a savage spectacle! I call for an encore!~
 END
 
 // Elven City
 // Tree of Life approach
 EXTEND_BOTTOM PLAYER1 33
-IF ~InParty("ZDBAE") InMyArea("ZDBAE") !StateCheck("ZDBAE",CD_STATE_NOTVALID) Global("ZDBAETreeOfLife","GLOBAL",0)~
+IF ~InParty("ZDBAE") InMyArea("ZDBAE") !StateCheck("ZDBAE",CD_STATE_NOTVALID) Global("ZDBAETreeOfLife","GLOBAL",0)~ 
 EXTERN PLAYER1 ZDBAETREE1
 END
 
 CHAIN PLAYER1 ZDBAETREE1
-@10146 /* Baeloth Barrityl, the drow sorcerer, an unlikely ally who has flung fire and unleashed his fury upon your foes. But is he truly prepared for the fight ahead? */
+~Baeloth Barrityl, the drow sorcerer, an unlikely ally who has flung fire and unleashed his fury upon your foes. But is he truly prepared for the fight ahead?~
 DO ~SetGlobal("ZDBAETreeOfLife","GLOBAL",1)~
 END
-++ @10147 /* Baeloth, we have become friends, so you don't have to follow me any further. This isn't your fight. */ EXTERN ZDBAEJ ZDBAETREE2
-++ @10148 /* There's a chance we won't survive. Turn tail and flee if you're too cowardly to see this battle through. */ EXTERN ZDBAEJ  ZDBAETREE2
-++ @10149 /* We're nearing the end. I have to know that you're ready. There's no turning back. */ EXTERN ZDBAEJ ZDBAETREE2
+++ ~Baeloth, we have become friends, so you don't have to follow me any further. This isn't your fight.~ EXTERN ZDBAEJ ZDBAETREE2
+++ ~There's a chance we won't survive. Turn tail and flee if you're too cowardly to see this battle through.~ EXTERN ZDBAEJ  ZDBAETREE2
+++ ~We're nearing the end. I have to know that you're ready. There's no turning back.~ EXTERN ZDBAEJ ZDBAETREE2
 
 
 CHAIN ZDBAEJ ZDBAETREE2
-@10150 /* I was preparing a particularly scorching fireball for these simpering elves. How they ever forced my kin beneath the ground is beyond comprehension. */
-== ZDBAEJ @10151 /* But yes, yes, I hear you. I have resolved to remain. The spotlight beckons and I shan't miss my cue! */
+~I was preparing a particularly scorching fireball for these simpering elves. How they ever forced my kin beneath the ground is beyond comprehension.~  
+== ZDBAEJ ~But yes, yes, I hear you. I have resolved to remain. The spotlight beckons and I shan't miss my cue!~
 END
-COPY_TRANS PLAYER1 33
+COPY_TRANS PLAYER1 33 
 
 // Tree of Life, Irenicus is dead
 I_C_T PLAYER1 16 ZDBAEIREN1
-== ZDBAEJ IF ~InParty("ZDBAE") Range("ZDBAE",15) !StateCheck("ZDBAE",CD_STATE_NOTVALID)~ THEN @10152 /* Obviously, this was going to happen. Our triumph was assured with my superior sorcerous skills. ...Ah! What is this strange, nay sinister, sensation? */
+== ZDBAEJ IF ~InParty("ZDBAE") Range("ZDBAE",15) !StateCheck("ZDBAE",CD_STATE_NOTVALID)~ THEN ~Obviously, this was going to happen. Our triumph was assured with my superior sorcerous skills. ...Ah! What is this strange, nay sinister, sensation?~
 END
