a5d43f367e2b0fe0ffde33c4de58dc1134546bc5
bucketfulofsunshine <AEShadowHunter@gmail.com>
2025-09-11 19:12:13 +0200

    Delete zdbae/dialog/FATESP.d

--- zdbae/dialog/FATESP.d
+++
@@ -1,7 +0,0 @@
-EXTEND_TOP FATESP 6 #2
-+ ~!Dead("ZDBAE") !InMyArea("ZDBAE") Global("ZDBAESummoned","GLOBAL",0)~ + ~Bring me Baeloth, the drow sorcerer.~
-DO ~CreateVisualEffect("SPPORTAL",[1999.1228])
-Wait(2) 
-CreateCreature("ZDBAE",[1999.1228],0) 
-SetGlobal("ZDBAESummoned","GLOBAL",1)~ GOTO 8
-END
